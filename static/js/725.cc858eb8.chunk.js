10030db87c9e045ce91411502cfdb89f8238cf22
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-05-15 20:02:17 +0000

    Deploying to gh-pages from @ W-Novytskyi/goit-react-hw-05-movies@702e97050d26e32cf2627c277fa581c15f7fee50 🚀

---
+++ static/js/725.cc858eb8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[725],{725:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("h2",{children:"Sorry. Requested page not found."})}}}]);
+//# sourceMappingURL=725.cc858eb8.chunk.js.map
